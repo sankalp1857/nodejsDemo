d1a0654da8f89ea8666013b80414251d6e85dacc
Monil Ladha <monil.ladha@reqruto.com>
2018-05-26 20:05:04 +0530

    Added first view page Index.ejs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejsDemo
 Demo app to learn nodejs and heroku
+### Running Locally
+$ node index.js
+### Deploying on Heroku
+$ git push heroku master
+$ heroku open
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.send("Hello, World!!");
+	res.render('index');
 });
 
 app.listen(port, () => {
