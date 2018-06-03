62cbcc2da5933d12ed05832a07e14f85d62625e8
Monil Ladha <monil.ladha@reqruto.com>
2018-06-04 01:18:58 +0530

    Code fix for taking dynamic URL

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  _url: String;
   result: any;
 
   constructor(private _http: HttpClient) {
+    console.log(window.location.origin);
+    this._url = window.location.origin;
   }
 
   addDataTestApi(value: Number) {
     let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     let body = { 'value': value };
-    return this._http.post('http://localhost:3000/add', body, { headers: headers }).pipe(
+    return this._http.post(this._url + '/add', body, { headers: headers }).pipe(
       map((res: Response) => this.result = res)
     );
   }
